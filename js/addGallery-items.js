0fd6d00094e763c572c26ee369ce87ae87ad29f8
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2022-11-07 18:30:50 +0200

    Add files via upload

---
+++ js/addGallery-items.js
@@ -0,0 +1,73 @@
+export const addGalleryItems = [
+  {
+    preview:
+      './img/forest-1072828_640.jpg',
+    original:
+      './img/forest-1072828_1280.jpg',
+    description: 'autumn forest',
+  },
+  {
+    preview:
+      './img/hd-wallpaper-1758197_640.jpg',
+    original:
+      './img/hd-wallpaper-1758197_1280.jpg',
+    description: 'autumn foliage',
+  },
+  {
+    preview:
+      './img/hd-wallpaper-83761_640.jpg',
+    original:
+      './img/hd-wallpaper-83761_1280.jpg',
+    description: 'Evening Pier',
+  },
+  {
+    preview:
+      './img/paris-7427636_640.jpg',
+    original:
+      './img/paris-7427636_1280.jpg',
+    description: 'Louvre',
+  },
+  {
+    preview:
+      './img/santa-claus-1819933_640.jpg',
+    original:
+      './img/santa-claus-1819933_1280.jpg',
+    description: 'Santa Claus',
+  },
+  {
+    preview:
+      './img/sea-164989_640.jpg',
+    original:
+      './img/sea-164989_1280.jpg',
+    description: 'sea sunset',
+  },
+  {
+    preview:
+      './img/tree-736885_640.jpg',
+    original:
+      './img/tree-736885_1280.jpg',
+    description: 'beaty sunset',
+  },
+  {
+    preview:
+      './img/winter-260817_640.jpg',
+    original:
+      './img/winter-260817_1280.jpg',
+    description: 'Snow',
+  },
+  {
+    preview:
+      './img/winter-landscape-2995987_640.jpg',
+    original:
+      './img/winter-landscape-2995987_1280.jpg',
+    description: 'winter sunset',
+    },
+  {
+    preview:
+      './img/woman-1274056_640.jpg',
+    original:
+      './img/woman-1274056_1280.jpg',
+    description: 'beatyfull woman',
+    },
+  
+];
